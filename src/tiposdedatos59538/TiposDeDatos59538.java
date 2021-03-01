af92a912ca88377b9921ae08a651e333a21ad68d
Personal <Personal@Familia>
2021-03-01 17:33:15 -0600

    TiposDeDatos59538

---
+++ src/tiposdedatos59538/TiposDeDatos59538.java
@@ -0,0 +1,42 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package tiposdedatos59538;
+
+/**
+ *
+ * @author JossmarOswaldoKuCupul
+ */
+public class TiposDeDatos59538 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Tipos de datos
+        
+        // byte
+        System.out.println("datoByteMínimo = " + Byte.MIN_VALUE);
+        System.out.println("datoByteMáximo = " + Byte.MAX_VALUE);
+        
+        // short
+        System.out.println("datoShortMínimo = " + Short.MIN_VALUE);
+        System.out.println("datoShortMáximo = " + Short.MAX_VALUE);
+        
+        // int
+        System.out.println("datoIntMínimo = " + Integer.MIN_VALUE);
+        System.out.println("datoIntMáximo = " + Integer.MAX_VALUE);
+        
+        // long
+        System.out.println("datoLongMínimo = " + Long.MIN_VALUE);
+        System.out.println("datoLongMáximo = " + Long.MAX_VALUE);
+        
+        // double
+        System.out.println("datoDoubleMínimo = " + Double.MIN_VALUE);
+        System.out.println("datoDoubleMáximo = " + Double.MAX_VALUE);
+    }
+    
+}
